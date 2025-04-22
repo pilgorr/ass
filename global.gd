73c1090372860c8ee1a3a6c67f97a9556e7c90b6
Ayush Thoren <ayushthoren@gmail.com>
2025-04-22 11:43:15 -0700

    Restructure project The repository now contains the project files, and the website is exported & deployed using Github Actions.
    Signed-off-by: Ayush Thoren <ayushthoren@gmail.com>

---
+++ global.gd
@@ -0,0 +1,17 @@
+extends Node
+var global_dead = false
+var global_sheep = 0
+
+signal sheepDie
+
+var sheepnum = 0
+
+var eliminated = ""
+
+var playername = "Nardo Polo"
+
+var currentlevel = ""
+
+var unlockedlevels = 0
+
+var beatenlevels = 0
